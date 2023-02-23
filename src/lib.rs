7c69dc5321ff28f7e092973deede9a2d8d6ec231
Adam Baranec <abaranec@gmail.com>
2023-02-23 18:57:37 +0100

    defining commands rules for models

--- src/lib.rs
+++ src/lib.rs
@@ -29,16 +29,17 @@ ENUMS
  */
 
 #[derive(PartialEq)]
-  pub enum Medium{
+  enum Medium{
     Visuals,
     Background,
     Audio,
     Mixed,
     Effect,
+    Multiplication,
     Unknown
   }
   #[derive(PartialEq)]
-  pub enum Variant{
+  enum Variant{
     Screen,
     Cube,
     Sphere,
@@ -55,6 +56,15 @@ ENUMS
     Multiplication,
     Unknown
   }
+  #[derive(PartialEq)]
+  enum TimeDomain{
+   Red,
+   Green,
+   Blue,
+   Scale,
+   Rotation,
+   Transform(f32,f32,f32)
+  }
 fn medium(word: &str)->Medium{
     if Regex::new("screen").unwrap().is_match(word){Medium::Background}
     else if Regex::new("cube").unwrap().is_match(word){Medium::Visuals}
@@ -64,7 +74,7 @@ fn medium(word: &str)->Medium{
     else if Regex::new("sqr").unwrap().is_match(word){Medium::Audio}
     else if Regex::new("tri").unwrap().is_match(word){Medium::Audio}
     else if Regex::new("rnd").unwrap().is_match(word){Medium::Audio}
-    else if Regex::new("mul").unwrap().is_match(word){Medium::Effect}
+    else if Regex::new("mul").unwrap().is_match(word){Medium::Multiplication}
     else if Regex::new("(lo|mi|hi)/g").unwrap().is_match(word){Medium::Mixed}
     else {Medium::Unknown}
 }
@@ -72,47 +82,48 @@ fn medium(word: &str)->Medium{
 INTERPRETING
  */
 
-fn floats_from(word: &str)->Result<Vec<f32>,&'static str>{
+fn floats_from(word: &str)->Option<Vec<f32>>{
     let mut floats:Vec<f32> = vec![];
+    let mut errors:u8 = 0;
     for float in Regex::new(r"(((0.(\d+)|1|0))|\-(0.(\d+)|1|0))").unwrap().find_iter(word){
       match String::from(float.as_str()).parse::<f32>(){
         Ok(val)=>floats.push(val),
-        Err(err)=>todo!(),
+        Err(err)=>errors += 1,
       }
     };
-    if floats.len() == 0{
-      Err("There are no floats in this parameter.")
-    } else {Ok(floats) }
+    if floats.len() == 0 || errors > 0{
+      None
+    } else {Some(floats) }
   }
 
-fn ra(word: &str)->Result<f32,()>{
+fn color(word: &str)->Option<[f32; 3]>{
     match word{
-        "small"=>Ok(0.25),
-        "medium"=>Ok(0.5),
-        "large"=>Ok(0.75),
-        "full"=>Ok(1.0),
-        _=>Err(())
+        "red"=>Some([1.0,0.0,0.0]),
+        "green"=>Some([0.0,1.0,0.0]),
+        "yellow"=>Some([1.0,1.0,0.0]),
+        "magenta"=>Some([1.0,0.0,1.0]),
+        "cyan"=>Some([0.0,1.0,1.0]),
+        "orange"=>Some([1.0,0.45,0.0]),
+        "pink"=>Some([1.0,0.6,0.8]),
+        "purple"=>Some([0.2,0.0,0.5]),
+        "brown"=>Some([0.3,0.2,0.1]),
+        "beige"=>Some([0.5,0.4,0.3]),
+        "black"=>Some([0.0,0.0,0.0]),
+        "white"=>Some([1.0,1.0,1.0]),
+        "grey"=>Some([0.3,0.3,0.3]),
+        "gray"=>Some([0.3,0.3,0.3]),
+        _=>None
     }
 }
 
-fn color(word: &str)->Result<[f32; 3],()>{
-    match word{
-        "red"=>Ok([1.0,0.0,0.0]),
-        "green"=>Ok([0.0,1.0,0.0]),
-        "yellow"=>Ok([1.0,1.0,0.0]),
-        "magenta"=>Ok([1.0,0.0,1.0]),
-        "cyan"=>Ok([0.0,1.0,1.0]),
-        "orange"=>Ok([1.0,0.45,0.0]),
-        "pink"=>Ok([1.0,0.6,0.8]),
-        "purple"=>Ok([0.2,0.0,0.5]),
-        "brown"=>Ok([0.3,0.2,0.1]),
-        "beige"=>Ok([0.5,0.4,0.3]),
-        "black"=>Ok([0.0,0.0,0.0]),
-        "white"=>Ok([1.0,1.0,1.0]),
-        "grey"=>Ok([0.3,0.3,0.3]),
-        "gray"=>Ok([0.3,0.3,0.3]),
-        _=>Err(())
-    }
+fn ra(word: &str)->Result<f32,()>{
+  match word{
+      "small"=>Ok(0.25),
+      "medium"=>Ok(0.5),
+      "large"=>Ok(0.75),
+      "full"=>Ok(1.0),
+      _=>Err(())
+  }
 }
 
 fn uv(word: &str)->Result<[f32; 2],()>{
@@ -122,11 +133,12 @@ match word{
 }
 }
 
-fn send_err(p: &Element, error: &str){
-    p.set_inner_html(error);
+fn send_err(p: &HtmlParagraphElement, error: &str){
+    let content = format!("{} {}", p.inner_html(), error);
+    p.set_inner_html(&content);
 }
 
-fn interpret(code: String){
+fn interpret(){
 let freq_regex = Regex::new(r"[\d]+\.[\d]+|[\d]+").unwrap();
 let note_regex = Regex::new(r"((c|d|e|f|g|a|b)(\d)|(c|d|e|f|g|a|b)(is|isis|es|eses)(\d))").unwrap();
 let gain_regex = Regex::new(r"((0.(\d+)|1|0)").unwrap();
@@ -135,34 +147,66 @@ let ra_regex = Regex::new(r"(0\.[\d]*$)|(1*$)|(0*$)").unwrap();
 let rgb_regex = Regex::new(r"rgb\((0.(\d+)|1|0),(0.(\d+)|1|0),(0.(\d+)|1|0)\)|[a-z]+").unwrap();
 let uv_regex = Regex::new(r"\[(((0.(\d+)|1|0))|\-(0.(\d+)|1|0)),(((0.(\d+)|1|0))|\-(0.(\d+)|1|0))\]|[a-z]+").unwrap();
 let rot_regex = Regex::new(r"rotate\((((0.(\d+)|1|0))|\-(0.(\d+)|1|0))\)").unwrap();
-let correct_commands: Vec<Vec<Regex>> = vec![];
+
+let mut freq:Option<f32> = None;
+let mut gain:Option<f32> = None;
+let mut pan:Option<f32> = None;
+let mut ra:Option<f32> = None;
+let mut rgb:Option<[f32; 3]> = None;
+let mut uv:Option<[f32; 2]> = None;
+let mut rot:Option<f32> = None;
+
 let document = web_sys::window().unwrap().document().unwrap();
-let error_p = document.get_element_by_id("error").unwrap();
-let comms: Vec<&str> = code.split(';').collect();
-for comm in comms{
-    let words: Vec<&str> = comm.split_whitespace().collect();
-    for word in &words{
-     if freq_regex.is_match(word){}
-     else if note_regex.is_match(word){}
-     else if gain_regex.is_match(word){}
-     else if pan_regex.is_match(word){}
-     else if ra_regex.is_match(word){}
-     else if rgb_regex.is_match(word){}
-     else if uv_regex.is_match(word){}
-     else if rot_regex.is_match(word){}
-     else {send_err(&error_p, "Invalid parameters.")}
-    }
-    if error_p.inner_html() == ""{
-        match medium(words[0]){
-            Medium::Background=>{},
-            Medium::Visuals=>{},
-            Medium::Audio=>{},
-            Medium::Effect=>{},
-            Medium::Unknown=>{},
-            _=>todo!()
-        }
-    }
-}
+let error_p = document.get_element_by_id("error").unwrap().dyn_into::<HtmlParagraphElement>().unwrap();
+let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<HtmlCanvasElement>().unwrap();
+let gl = canvas.get_context("webgl2").unwrap().unwrap().dyn_into::<WebGl2RenderingContext>().unwrap();
+let code = document.get_element_by_id("input").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap().value();
+let gl = canvas.get_context("webgl2").unwrap().unwrap().dyn_into::<WebGl2RenderingContext>().unwrap();
+
+error_p.set_inner_html("");
+
+if !code.is_empty(){
+  let comms: Vec<&str> = code.split(';').collect();
+  for comm in comms{
+      let expr: Vec<&str> = comm.split_whitespace().collect();
+      if medium(expr[0]) == Medium::Unknown{ send_err(&error_p, "Unknown media. Allowed: screen, cube, saw... More in help"); }
+        else if medium(expr[0]) == Medium::Background && expr.len() == 1{ send_err(&error_p, "Missing color"); } 
+        else if medium(expr[0]) == Medium::Background && expr.len() > 2{ send_err(&error_p, "Too many parameters, color is enough"); } 
+        else if medium(expr[0]) == Medium::Visuals && expr.len() > 5{ send_err(&error_p, "More than 4 parameters are not provided for models"); } 
+        else if medium(expr[0]) == Medium::Audio && expr.len() == 1{ send_err(&error_p, "Missing frequency for the oscillator."); } 
+        else if medium(expr[0]) == Medium::Audio && expr.len() > 3{ send_err(&error_p, "More than 3 parameters are not provided for oscillators"); } 
+        else if medium(expr[0]) == Medium::Multiplication && expr.len() == 1{ send_err(&error_p, "Number of rows and number of columns is needed"); } 
+        else if medium(expr[0]) == Medium::Multiplication && expr.len() > 3{ send_err(&error_p, "Other parameters are not needed for scissoring the canvas"); } 
+        else {
+        for word in &expr{
+          if freq_regex.is_match(word){ match String::from(*word).parse::<f32>(){ Ok(f)=>{freq = Some(f);}, Err(e)=>{}} }
+          else if note_regex.is_match(word){}
+          else if gain_regex.is_match(word){ match String::from(*word).parse::<f32>(){ Ok(g)=>{gain = Some(g);}, Err(e)=>{}} }
+          else if pan_regex.is_match(word){ match String::from(*word).parse::<f32>(){ Ok(p)=>{pan = Some(p);}, Err(e)=>{}} }
+          else if ra_regex.is_match(word){ match String::from(*word).parse::<f32>(){ Ok(r)=>{ra = Some(r);}, Err(e)=>{}} }
+          else if rgb_regex.is_match(word){ 
+            if rgb_regex.is_match(r"[a-z]+") && rgb_regex.find(r"(0.(\d+)|1|0))") == None{
+              if color(word) != None {rgb = color(word);}
+            } else {
+              let arr = floats_from(*word); if arr != None {rgb = Some([arr.clone().unwrap()[0], arr.clone().unwrap()[1], arr.clone().unwrap()[2]]);} else {} 
+            }
+          }
+          else if uv_regex.is_match(word){ let arr = floats_from(*word); if arr != None {uv = Some([arr.clone().unwrap()[0], arr.clone().unwrap()[1]]);} else {} }
+          else if rot_regex.is_match(word){ let arr = floats_from(*word); if arr != None {rot = Some(arr.clone().unwrap()[0]);} else {} }
+          else {send_err(&error_p, "Invalid parameters.")}
+         }
+         if error_p.inner_html() == ""{
+             match medium(expr[0]){
+                 Medium::Background=>{ if rgb != None { clear(&gl, rgb.unwrap()); } else { send_err(&error_p, "Invalid screen color."); } },
+                 Medium::Visuals=>{},
+                 Medium::Audio=>{},
+                 Medium::Effect=>{},
+                 _=>todo!()
+             }
+         }
+      }
+  }
+} else { clear(&gl, [0.0,0.0,0.0]); }
 }
 
 /*
@@ -173,7 +217,7 @@ gl.clear_color(color[0], color[1], color[2], 1.0);
 gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 }
 
-fn render(){
+fn render(models: &[&dyn Object]){
     let canvas = web_sys::window().unwrap().document().unwrap().get_element_by_id("canvas").unwrap()
     .dyn_into::<HtmlCanvasElement>().unwrap();
     let gl = canvas.get_context("webgl2").unwrap().unwrap().dyn_into::<WebGl2RenderingContext>().unwrap();
@@ -192,9 +236,8 @@ fn render(){
         1000.0
         );
     let light = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0,0.0,0.0));
-    let objects:Vec<&dyn Object> = vec![];
+
     window.render_loop(move |frame_input: FrameInput|{
-        frame_input.screen().clear(ClearState::color(0.0,0.0,0.0,1.0));
         FrameOutput::default()
     });
 }
@@ -217,9 +260,5 @@ MAIN FUNCTIONS FOR WASM
  */
 #[wasm_bindgen]
 pub fn set(mut code: String, audio: AudioContext){
-}
-#[wasm_bindgen]
-pub fn draw(){
-    render();
 }
   

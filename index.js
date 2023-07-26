b7c26183e44d9919997f954b4499c74ff5127ebf
Adam Baranec <abaranec@gmail.com>
2023-07-26 19:03:08 +0200

    some trial

--- index.js
+++ index.js
@@ -369,17 +369,22 @@ if (typeof c === 'string'){
           texture.needsUpdate = true;
           texture.generateMipmaps = true;
           material = new THREE.MeshPhongMaterial({ map: texture});
-        } /*else if (command[i].startsWith("tex(") && command[i].endsWith(")")) {
+        } else if (command[i].startsWith("tex(") && command[i].endsWith(")")) {
           let url = command[i].slice(5,command[i].length - 2);
           if (isValidUrl(url)){
             const textureLoader = new THREE.TextureLoader();
             textureLoader.setCrossOrigin('anonymous');
-            const texture = textureLoader.load(url);
-            material = new THREE.MeshPhongMaterial({ map: texture});
+            const texture = textureLoader.load(url, (texture) => {
+              texture.needsUpdate = true;
+              texture.generateMipmaps = true;
+              material = new THREE.MeshPhongMaterial({ map: texture});
+            }, undefined, (err) => {
+              sendErr(err);
+            });
           } else {
             sendErr("Invalid URL");
           }
-        }*/ else {
+        } else {
           sendErr("Unknown parameter. Allowed: radius, color, rotation, texture...");
         }
       }

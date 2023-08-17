281ba5c5ca624c791197900c917e2ae38444c38a
Adam Baranec <abaranec@gmail.com>
2023-08-17 14:37:45 +0200

    another trial

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 /* DO NOT NOTICE THIS FILE. JUST FOR TESTING PURPOSES */
 
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
